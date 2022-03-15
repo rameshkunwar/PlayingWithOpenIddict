32f53bbc8a250ef528d9fa8734ef69deae6b2178
Ramesh Kunwar <rameshkunwar@hotmail.com>
2022-03-15 22:31:01 +0100

    login page, account controller, _ViewImports & _ViewStart location fixed.

--- AuthorizationServer/Program.cs
+++ AuthorizationServer/Program.cs
@@ -1,12 +1,19 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 //Add services to the container, configure MVC as we used an empty web template
 builder.Services.AddControllersWithViews();
 
+//enable authentication, cookie based
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = "/account/login";
+    });
+
 WebApplication? app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-
-
 
 //configure the http request pipeline
 if (!app.Environment.IsDevelopment())
@@ -14,8 +21,10 @@ if (!app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
 
 app.Run();
---
+++ AuthorizationServer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthorizationServer.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = "username";
+        [Required]
+        public string Password { get; set; } = "p@$$Wørd";
+        public string ReturnUrl { get; set; } = string.Empty;
+    }
+}
